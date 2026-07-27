6a4997d998d098d4643178ea1ec04ea9bc9dd043
christina <158211764+christinac09@users.noreply.github.com>
2026-07-27 12:10:50 -0400

    idk if this is right

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -5,7 +5,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const getHomeRoute = () =>
     userStore.user?.userType === "teacher" ? "/teacher/classes" : "/student/home";
 
-  // Redirect teacher home to classes
+  // redirect /teacher/home to /teacher/classes
   if (to.path === "/teacher/home") {
     return navigateTo("/teacher/classes", { redirectCode: 301 });
   }
@@ -14,8 +14,6 @@ export default defineNuxtRouteMiddleware(async (to) => {
     return navigateTo(getHomeRoute(), { redirectCode: 301 });
   }
 
-  // Only run on initial page load
-  // https://nuxt.com/docs/guide/directory-structure/middleware#when-middleware-runs
   if (
     !import.meta.client || !nuxtApp.isHydrating || !nuxtApp.payload.serverRendered
   ) {
