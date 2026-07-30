6e4a0a9a464f5da947feee57d7d8ddace30581b2
christina chen <158211764+christinac09@users.noreply.github.com>
2026-07-30 10:23:35 -0400

    Update home.vue

--- app/pages/student/home.vue
+++ app/pages/student/home.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>
-        <h1>Student Homepage</h1>
-    </div>
+  <div>
+    <h1>Student Homepage</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-    layout: "student"
+  layout: "student",
+  requiresAuth: true,
+  redirectIfAuth: false,
+  middleware: "role-check",
+  allowedRoles: ["student"],
 });
 </script>
 
