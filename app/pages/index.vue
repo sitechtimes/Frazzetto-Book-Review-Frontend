580e1705e6a8c8469fb84e510739d1148f3f1654
christina chen <158211764+christinac09@users.noreply.github.com>
2026-07-30 10:35:10 -0400

    index is login

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
-    <h1>(landing page...) Welcome to ...</h1>
-    <NuxtLink to="/login" class="btn btn-primary"> Login </NuxtLink>
+  <div
+    class="bg-base-200 text-base-content flex min-h-screen items-center justify-center px-4"
+  >
+    <div
+      class="card bg-base-100 border-base-300 w-full max-w-full rounded-lg border p-4 shadow-md sm:max-w-md sm:p-6 md:max-w-lg md:p-6 lg:max-w-xl lg:p-8"
+    >
+      <form class="flex flex-col space-y-4" @submit.prevent="handleLogin">
+        <div>
+          <label for="email" class="mb-1 block font-medium"
+            >Enter your email:</label
+          >
+          <input
+            v-model="loginForm.email"
+            type="text"
+            placeholder="email@example.com"
+            class="input input-bordered bg-base-100 w-full"
+          />
+        </div>
+
+        <div>
+          <label for="pin" class="mb-1 block font-medium"
+            >Enter your pin:</label
+          >
+          <input
+            v-model.number="loginForm.pin"
+            type="password"
+            placeholder="••••••••"
+            class="input input-bordered bg-base-100 w-full"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary w-full">Login</button>
+
+        <p v-if="errorMessage" class="text-error mt-2 text-sm">
+          {{ errorMessage }}
+        </p>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -10,6 +45,57 @@ definePageMeta({
   requiresAuth: false,
   redirectIfAuth: true,
 });
+
+const errorMessage = ref("");
+const store = useUserStore();
+const router = useRouter();
+
+const loginForm = reactive({
+  email: "",
+  pin: "",
+});
+
+watch(
+  () => loginForm.email,
+  (newEmail) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!newEmail) {
+      errorMessage.value = "";
+      return;
+    }
+
+    if (!emailRegex.test(newEmail)) {
+      errorMessage.value = "Invalid email format";
+    } else {
+      errorMessage.value = "";
+    }
+  },
+);
+
+async function handleLogin() {
+  if (!loginForm.email || loginForm.pin === null) {
+    errorMessage.value = "Email and PIN are required";
+    return;
+  }
+
+  if (errorMessage.value) return;
+
+  errorMessage.value = "";
+
+  const { data, error } = await store.signIn(loginForm.email, loginForm.pin);
+
+  if (error) {
+    errorMessage.value = error.message;
+    return;
+  }
+
+  if (data.is_student) {
+    await router.push("/student/home");
+  } else if (data.is_teacher) {
+    await router.push("/teacher/classes");
+  }
+}
 </script>
 
 <style scoped></style>
